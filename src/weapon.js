0ef120e87ba732a4227eb6be6a0d14e9942106f8
chenqy9 <qingyao.chen@meicloud.com>
2018-07-26 09:11:17 +0800

    增加插件

--- src/weapon.js
+++ src/weapon.js
@@ -9,11 +9,12 @@ const weapon = {}
  * @returns {String} value
  */
 function getUrlParam (name) {
-  var reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)') // 构造一个含有目标参数的正则表达式对象
-  var r = window.location.search.substr(1).match(reg) // 匹配目标参数
+  const reg = new RegExp(`(^|&)${name}=([^&]*)(&|$)`) // 构造一个含有目标参数的正则表达式对象
+  const r = window.location.search.substr(1).match(reg) // 匹配目标参数
   if (r != null) {
     return unescape(r[2]) // 返回参数值
   }
+  return null
 }
 
 /**
@@ -24,34 +25,33 @@ function getUrlParam (name) {
  */
 function color2Rgba (color, opacity) {
   // 十六进制颜色值的正则表达式
-  var reg = /^#([0-9a-fA-f]{3}|[0-9a-fA-f]{6})$/
+  const reg = /^#([0-9a-fA-f]{3}|[0-9a-fA-f]{6})$/
   // 如果参数不是有效的16进制颜色
   if (typeof color !== 'string' && reg.test(color)) {
     return color
-  } else {
-    let sColor = color.toLowerCase()
-    if (sColor.length === 4) {
-      let sColorNew = '#'
-      for (let i = 1; i < 4; i += 1) {
-        sColorNew += sColor.slice(i, i + 1).concat(sColor.slice(i, i + 1))
-      }
-      sColor = sColorNew
-    }
-    // 处理六位的颜色值
-    var sColorChange = []
-    for (var i = 1; i < 7; i += 2) {
-      sColorChange.push(parseInt('0x' + sColor.slice(i, i + 2)))
-    }
-    let opacityNew = parseFloat(opacity)
-    if (isNaN(opacityNew) || opacityNew < 0 || opacityNew > 1) {
-      opacityNew = 1
-    }
-    return 'rgba(' + sColorChange.join(',') + ',' + opacityNew + ')'
   }
+  let sColor = color.toLowerCase()
+  if (sColor.length === 4) {
+    let sColorNew = '#'
+    for (let i = 1; i < 4; i += 1) {
+      sColorNew += sColor.slice(i, i + 1).concat(sColor.slice(i, i + 1))
+    }
+    sColor = sColorNew
+  }
+  // 处理六位的颜色值
+  const sColorChange = []
+  for (let i = 1; i < 7; i += 2) {
+    sColorChange.push(parseInt(`0x${sColor.slice(i, i + 2)}`))
+  }
+  let opacityNew = parseFloat(opacity)
+  if (isNaN(opacityNew) || opacityNew < 0 || opacityNew > 1) {
+    opacityNew = 1
+  }
+  return `rgba(${sColorChange.join(',')},${opacityNew})`
 }
 
 function formatNumber (num, precision, separator) {
-  var parts
+  let parts
   // 判断是否为数字
   if (!isNaN(parseFloat(num)) && isFinite(num)) {
     // 把类似 .5, 5. 之类的数据转化成0.5, 5, 为数据精度处理做准, 至于为什么
@@ -64,7 +64,7 @@ function formatNumber (num, precision, separator) {
     // 分离数字的小数部分和整数部分
     parts = num.split('.')
     // 整数部分加[separator]分隔, 借用一个著名的正则表达式
-    parts[0] = parts[0].toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1' + (separator || ','))
+    parts[0] = parts[0].toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, `$1${separator || ','}`)
 
     return parts.join('.')
   }
@@ -74,16 +74,12 @@ function formatNumber (num, precision, separator) {
 /**
  * @description 判断客户端是否iphone
  */
-const isIPhone = () => {
-  return window.navigator.appVersion.match(/iphone/gi)
-}
+const isIPhone = () => window.navigator.appVersion.match(/iphone/gi)
 
 /**
  * @description 判断客户端是否android
  */
-const isAndroid = () => {
-  return window.navigator.appVersion.match(/android/gi)
-}
+const isAndroid = () => window.navigator.appVersion.match(/android/gi)
 
 weapon.getUrlParam = getUrlParam
 weapon.formatNumber = formatNumber
